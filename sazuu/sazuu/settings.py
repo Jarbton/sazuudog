e73372aacb0547eac8871019f8835ee4fd3a5c37
Jacob Barton <j.barton.mail@googlemail.com>
2020-09-24 21:51:38 +0100

    Heroku stuff

--- sazuu/sazuu/settings.py
+++ sazuu/sazuu/settings.py
@@ -109,8 +109,16 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+# Database connection
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage' 
